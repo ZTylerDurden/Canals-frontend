efc363f8c869c6873a441b63343be2acf031a8cb
Pablo Gomez <gomezpa@bellsouth.net>
2018-03-20 17:46:55 -0400

    Styling welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { AuthService } from "./services/auth.service";
+import { CanalService } from "./services/canal.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = '';
+
+  logoutError: string;
+
+  constructor(
+    private myAuthService: AuthService,
+    private myRouter: Router,
+    private myCanalService: CanalService
+  ) {}
+  
+  logMeOutPls() {
+    this.myAuthService
+      .logout()
+      .then(() => {
+        this.myRouter.navigate(["/"]);
+      })
+      .catch(() => {
+        this.logoutError = "Log out went bad.";
+      });
+  } // close logMeOutPls()
+
 }
